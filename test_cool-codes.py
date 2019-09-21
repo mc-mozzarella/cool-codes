511519853a3b5a7ff4d760600aaa0bc1ccac5138
mc_mozzarella <whydoineedamail>
2019-09-21 14:39:39 +0200

    Added test for multiple_inheritance.py

--- test_cool-codes.py
+++ test_cool-codes.py
@@ -1,3 +1,4 @@
+import pytest
 from fractions import simplify_fraction
 def test_simplify_fraction():
     assert simplify_fraction(2,4) == (1,2)
@@ -15,4 +16,45 @@ def test_make_last_false():
     assert list(make_last_false(0)) == [False]
     assert list(make_last_false(1)) == [True, False]
     assert list(make_last_false(5)) == [True, True, True, True, True, False]
-    assert make_last_false_while(3)
+
+from multiple_inheritance import Clock
+def test_clock():
+    time = Clock(23,1,1)
+    assert str(time) == '23:1:1'
+    time.tick()
+    assert repr(time) == 'Clock(23, 1, 2)'
+
+    with pytest.raises(Exception) as e_time:
+        Clock(24,1,1)
+        Clock(23,60,1)
+        Clock(23,45,60)
+    
+
+from multiple_inheritance import Calendar
+def test_calendar():
+    date = Calendar(1984, 4, 17)
+    assert str(date) == '17.4.1984'
+    Calendar.date_style = 'American'
+    assert str(date) == '4.17.1984'
+    date.advance()
+    assert repr(date) == 'Calendar(1984, 4, 18)'
+
+    with pytest.raises(Exception) as e_date:
+        Calendar(1984, 13, 17)
+        Calendar(1984, 2, 30)
+
+    # set date_style back to default
+    Calendar.date_style = 'British'
+
+from multiple_inheritance import CalendarClock
+def test_calendarclock():
+    time = CalendarClock(1984, 4, 17, 14, 37, 21)
+    assert str(time) == '14:37:21 17.4.1984'
+    assert repr(time) == 'CalendarClock(1984, 4, 17, 14, 37, 21)'
+
+    with pytest.raises(Exception) as e_timedate:
+        CalendarClock(1984, 13, 17, 14, 37, 21)
+        CalendarClock(1984, 4, 43, 14, 37, 21)
+        CalendarClock(1984, 4, 17, 34, 37, 21)
+        CalendarClock(1984, 4, 17, 14, 60, 21)
+        CalendarClock(1984, 4, 17, 14, 37, 70)
